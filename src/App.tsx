9c4561247ae98602892fad9f3ae1a195f3e0ee74
Mil0w0 <lhilderal@myges.fr>
2024-04-09 23:30:12 +0200

    creating web app base

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './images/logo.png';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Arcadia Solutions - Back office template </h2>
+        <p>Why is the logo animated ? Good question.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
