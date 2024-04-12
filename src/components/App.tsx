27d3fc4bbafec5a9fe0cc41f970e0fac58ebcf51
Mil0w0 <lhilderal@myges.fr>
2024-04-12 19:52:44 +0200

    new Header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import logo from './images/logo.png';
-import './styles/App.css';
+import '../styles/App.css';
+import Header from "./Header";
 
 function App() {
-  return (
-    <div className="App">
+
+    return (
+      <body>
+          <Header />
+      </body>
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Arcadia Solutions - Back office template </h2>
         <p>Why is the logo animated ? Good question.</p>
       </header>
-    </div>
+    </div>*/
+
+
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import '../styles/Header.css';
+export default function Header() {
+    return (
+      <div className="main-header">
+        <h1>My App header test</h1>
+      </div>
+    )
+  }
+  
