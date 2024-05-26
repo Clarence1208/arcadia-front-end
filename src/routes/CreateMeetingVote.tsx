a2797b1539ffec5df580272d976c162a437d894a
Nino <ninoplane@gmail.com>
2024-05-26 22:14:03 +0200

    fix votes back

--- src/routes/CreateMeetingVote.tsx
+++ src/routes/CreateMeetingVote.tsx
@@ -8,6 +8,7 @@ import Header from "./components/Header";
 import { Footer } from "./components/Footer";
 import HelpIcon from '@mui/icons-material/Help';
 import AddBoxIcon from '@mui/icons-material/AddBox';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 type CreateVoteData = {
     name: string,
@@ -54,6 +55,10 @@ function CreateMeetingVoteForm() {
     }
 
     function setVotesIfNotAlready(votes: string[], name: string) {
+        if(name === "") {
+            setErrorMessage("Erreur : Le choix ne peut pas être vide")
+            return
+        }
         if (!votes.includes(name)) {
             setVotes([...votes, name]);
         } else {
@@ -61,8 +66,37 @@ function CreateMeetingVoteForm() {
         }
     }
 
+    function deleteVoteChoice(vote: string, key: number) {
+        const newVotes = votes.filter((_, index) => index !== key);
+        setVotes(newVotes);
+    }
+
+    function changeAbsoluteMajority() {
+        setData(prev => {
+            return { ...prev, isAbsoluteMajority: !prev.isAbsoluteMajority }
+        })
+    }
+
+    function changeAnonymous() {
+        setData(prev => {
+            return { ...prev, isAnonymous: !prev.isAnonymous }
+        })
+    }
+
+    function checkFields(): boolean {
+        if(votes.length < 2) {
+            setErrorMessage("Erreur : Il doit y avoir au moins 2 choix")
+            return false
+        }
+        return true
+    }
+
     async function onSubmit(e: FormEvent) {
         e.preventDefault()
+        const isValid = checkFields()
+        if (!isValid) {
+            return
+        }
         const response: Response = await fetch( process.env.REACT_APP_API_URL+"/meetings", {method: "POST", body: JSON.stringify(body), headers: {"Content-Type": "application/json"}});
         if (!response.ok) {
             const error =  await response.json()
@@ -137,11 +171,11 @@ function CreateMeetingVoteForm() {
                 </div>
                 <div>
                     <InputLabel>Majorité absolue</InputLabel>
-                    <Switch checked={data.isAbsoluteMajority} color="primary" />
+                    <Switch checked={data.isAbsoluteMajority} onChange={e => changeAbsoluteMajority()} color="primary" />
                 </div>
                 <div>
                     <InputLabel>Vote Anonyme</InputLabel>
-                    <Switch checked={data.isAnonymous} color="primary" />
+                    <Switch checked={data.isAnonymous}  onChange={e => changeAnonymous()} color="primary" />
                 </div>
                 <div className="add-choice">
                     <TextField 
@@ -154,9 +188,9 @@ function CreateMeetingVoteForm() {
                     <Button variant="contained" color="primary" endIcon={<AddBoxIcon />} sx={{marginLeft: 2}} onClick={() => {setVotesIfNotAlready(votes, name)}}>Ajouter Choix</Button>
                 </div>
                 <div>
-                    {votes.map((vote) => (
+                    {votes.map((vote, key) => (
                         <div>
-                            <span>- {vote}</span>
+                            <li>{key +1} : {vote} <Button variant="outlined" color="secondary" endIcon={<DeleteIcon color="primary" />} onClick={() => {deleteVoteChoice(vote, key)}}></Button></li>
                         </div>
                     ))}
                 </div>
@@ -165,9 +199,7 @@ function CreateMeetingVoteForm() {
                 variant="contained" 
                 color="primary"
                 onClick={onSubmit}
-                >
-                    Soumettre
-                </Button>
+                >Soumettre</Button>
             </form>
         </div>
     );
