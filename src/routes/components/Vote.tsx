0d734d27159c31b978e6f4a1d78a33f4750138a3
Nino <ninoplane@gmail.com>
2024-06-02 18:19:43 +0200

    some more vote conditions and wip on endVote

--- src/routes/components/Vote.tsx
+++ src/routes/components/Vote.tsx
@@ -3,10 +3,19 @@ import '../../styles/Vote.css';
 import {UserSessionContext} from "../../contexts/user-session";
 import { useContext } from "react";
 import HowToVoteIcon from '@mui/icons-material/HowToVote';
+import TaskIcon from '@mui/icons-material/Task';
 
 interface VoteProps {
     vote: Vote;
     meetingId?: number;
+}
+
+interface User {
+    id: number,
+    firstName: string,
+    lastName: string,
+    email: string,
+    roles: string[],
 }
 
 interface Vote {
@@ -19,21 +28,53 @@ interface Vote {
     nbPossibleVotes: number,
     quorum?: number,
     currentRound?: number,
+    users?: User[],
 }
 
 export function Vote({vote, meetingId}: VoteProps){
     const userSession = useContext(UserSessionContext)?.userSession
+    if(vote.users === undefined){
+        vote.users = []
+    }
+    const userInVote = vote.users.find(user => user.id === userSession?.userId)
+    const voteCanEnd = vote.quorum ? vote.users.length >= vote.quorum : true
+
     return (
         <div className="vote-div" >
             <ListItemText 
                 primary={vote.name} 
             />
-            <Button 
-            variant="contained" 
-            color="primary" 
-            href={`/meeting/${meetingId}/vote/${vote.id}`}
-            endIcon={ <HowToVoteIcon></HowToVoteIcon>}
-            >Voter</Button>
+            {(userSession?.roles.includes("admin") || userSession?.roles.includes("superadmin")) && vote.quorum ? 
+            <div>
+                <span className="vote-admin">Quorum : {vote.users?.length}/{vote.quorum}</span>
+            </div> : <></>
+                }
+            {!userInVote ?
+                <Button 
+                variant="contained" 
+                color="primary" 
+                href={`/meeting/${meetingId}/vote/${vote.id}`}
+                endIcon={ <HowToVoteIcon></HowToVoteIcon>}
+                >Voter</Button>
+            : <Button
+                variant="contained"
+                color="primary"
+                endIcon={<TaskIcon></TaskIcon>}
+                disabled
+            >Vote Fait</Button>
+            }
+            <br/>
+            {userSession && (userSession.roles.includes("admin") || userSession.roles.includes("superadmin")) ? (
+                voteCanEnd ? (
+                    <Button variant="contained" color="primary" onClick={() => {}}>
+                        Mettre fin au Vote
+                    </Button>
+                ) : (
+                    <Button variant="contained" color="primary" onClick={() => {}} disabled>
+                        Mettre fin au Vote
+                    </Button>
+                )
+            ) : null}
         </div>
     )
 }
