9a1e039d7ce9d94a5b1e02637f70b914e78cab2f
Mil0w0 <lhilderal@myges.fr>
2024-06-08 00:19:53 +0200

    adding stripe lib and tab panel

--- src/routes/AdminDashboard.tsx
+++ src/routes/AdminDashboard.tsx
@@ -1,26 +1,24 @@
 import Header from "./components/Header";
 import {Footer} from "./components/Footer";
-import {SyntheticEvent, useContext, useState} from "react";
+import React, {SyntheticEvent, useContext, useState} from "react";
 import {UserSessionContext} from "../contexts/user-session";
-/*import {WebsitesPanel} from "../components/features/dashboard/WebsitesPanel";
-import {UserAccountPanel} from "../components/features/dashboard/UserAccountPanel";*/
 import {Button, Tab, Tabs} from "@mui/material";
 import "../styles/Dashboard.css";
 import {UsersDashboard} from "./UsersDashboard";
-import { MeetingsList } from "./meetings/MeetingsList";
+import {MeetingsList} from "./meetings/MeetingsList";
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import {WebsiteSettings} from "./WebsiteSettings";
 import {PollList} from "./polls/PollList";
 
-//tabs comes from MUI API docs https://mui.com/material-ui/react-tabs/
 function a11yProps(index: number) {
     return {
         id: `vertical-tab-${index}`,
         'aria-controls': `vertical-tabpanel-${index}`,
     };
 }
+
 function TabPanel(props: any) {
-    const { children, value, index, ...other } = props;
+    const {children, value, index, ...other} = props;
 
     return (
         <div
@@ -39,7 +37,7 @@ function TabPanel(props: any) {
     );
 }
 
-export function Dashboard(){
+export function Dashboard() {
     const userSession = useContext(UserSessionContext)?.userSession
     const [tabsValue, setTabsValue] = useState(0);
 
@@ -49,7 +47,7 @@ export function Dashboard(){
     };
     return (
         <div>
-            <Header />
+            <Header/>
 
             <div id="dahsboard-main" className="main">
 
@@ -60,42 +58,48 @@ export function Dashboard(){
                     value={tabsValue}
                     onChange={handleChange}
                     aria-label="Vertical tabs example"
-                    sx={{ borderRight: 1, borderColor: 'divider' }}
+                    sx={{borderRight: 1, borderColor: 'divider'}}
                 >
                     <Tab label="Gestion des Assemblées Générales" {...a11yProps(0)}/>
                     <Tab label="Gestion des utilisateurs" {...a11yProps(1)}/>
                     <Tab label="Gestions des événements" {...a11yProps(2)}/>
                     <Tab label="Gestions des paramètres globaux du site" {...a11yProps(3)}/>
                     <Tab label="Gestions des sondages" {...a11yProps(4)}/>
+                    <Tab label="Gestions des dons" {...a11yProps(5)}/>
                 </Tabs>
 
                 <div className={"board"}>
                     <h1>Tableau de bord de {userSession?.fullName || "l'administrateur"}</h1>
                     <TabPanel value={tabsValue} index={0} className={"tab-panel"}>
-                        <Button href="/createMeeting" variant="contained" color="primary" endIcon={ <AddBoxIcon></AddBoxIcon>}>Créer une nouvelle assemblée générale</Button>
-                            <MeetingsList />
+                        <Button href="/createMeeting" variant="contained" color="primary"
+                                endIcon={<AddBoxIcon></AddBoxIcon>}>Créer une nouvelle assemblée générale</Button>
+                        <MeetingsList/>
                     </TabPanel>
                     <TabPanel value={tabsValue} index={1}>
-                        <UsersDashboard />
-                        {/*<UserAccountPanel userId={userSession?.userId} userToken={userSession?.loginToken} />*/}
+                        <UsersDashboard/>
                     </TabPanel>
                     <TabPanel value={tabsValue} index={2}>
                         <h1>2</h1>
                     </TabPanel>
                     <TabPanel value={tabsValue} index={3}>
-                        <WebsiteSettings />
+                        <WebsiteSettings/>
                     </TabPanel>
                     <TabPanel value={tabsValue} index={4}>
-                    <Button href="/createPoll" variant="contained" color="primary" endIcon={ <AddBoxIcon></AddBoxIcon>}>Créer un nouveau sondage</Button>
+                        <Button href="/createPoll" variant="contained" color="primary"
+                                endIcon={<AddBoxIcon></AddBoxIcon>}>Créer un nouveau sondage</Button>
                         <div className={"board"}>
-                            <PollList />
+                            <PollList/>
                         </div>
                     </TabPanel>
+                    <TabPanel value={tabsValue} index={4}>
+                        <h2>Lien à Stripe: </h2>
+                        <p>Vous pouvez gérer le lien de paiement Stripe d'ici</p>
+                    </TabPanel>
+
                 </div>
 
             </div>
-
-            <Footer />
+            <Footer/>
         </div>
     );
 }
