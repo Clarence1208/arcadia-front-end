254d4e28979a20a4abafad9a231d3d8db70a20f0
Clarence HIRSCH <chirsch1@myges.fr>
2024-06-16 17:47:10 +0200

    trying migrating to custom global variable injection V11

--- src/config/fetchConfig.ts
+++ src/config/fetchConfig.ts
@@ -4,15 +4,14 @@ const fetchConfig = async (): Promise<AppConfig> => {
     const response = await fetch('/usr/share/nginx/html/config.json');  // Ensure this URL is correct
     //log the response body
     console.log(await response.text());
-    throw new Error(await response.text());
-    // if (!response.ok) {
-    //     throw new Error(`Failed to fetch configuration: ${response.status} ${response.statusText}`);
-    // }
-    // const contentType = response.headers.get('content-type');
-    // if (!contentType || !contentType.includes('application/json')) {
-    //     throw new TypeError("Oops, we haven't got JSON!");
-    // }
-    // return response.json() as Promise<AppConfig>;
+    if (!response.ok) {
+        throw new Error(`Failed to fetch configuration: ${response.status} ${response.statusText}`);
+    }
+    const contentType = response.headers.get('content-type');
+    if (!contentType || !contentType.includes('application/json')) {
+        throw new TypeError("Oops, we haven't got JSON!");
+    }
+    return response.json() as Promise<AppConfig>;
 }
 
 
