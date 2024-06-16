0bfc4cfedcd5faa2ccbd83d20628de37b8bccb76
Clarence HIRSCH <chirsch1@myges.fr>
2024-06-16 17:09:45 +0200

    trying migrating to custom global variable injection V6

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,7 @@
 export interface AppConfig {
     associationName: string;
+    port: number
     apiURL: string;
+    openaiAPIKey: string
     stripePublicKey: string;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,50 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import theme from "./utils/theme";
-import {ThemeProvider} from "@mui/material";
+import {AppConfig} from "./config/config";
+import fetchConfig from "./config/fetchConfig";
 
-export const ConfigContext = createContext<any>(null);
+export const ConfigContext = createContext<AppConfig | null>(null);
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
-const fetchConfig = async (): Promise<any> => {
-    try {
-        const response = await fetch('/usr/share/nginx/html/config.json');
-        if (!response.ok) {
-            throw new Error('Failed to fetch configuration');
-        }
-        return await response.json();
-    } catch (error) {
-        console.error("Error fetching configuration:", error);
-        return null;
-    }
-};
-
 const RootComponent = () => {
-    const [config, setConfig] = useState<any>(null);
+    const [config, setConfig] = useState<AppConfig | null>(null);
 
     useEffect(() => {
         fetchConfig().then(data => {
             setConfig(data);
+        }).catch(error => {
+            console.error("Error fetching configuration:", error);
         });
     }, []);
 
-    // if (!config) {
-    //     return <div>Loading configuration...</div>; // Optionally add a spinner here
-    // }
+    if (!config) {
+        return <div>Loading configuration...</div>; // Optionally add a spinner here
+    }
 
     return (
         <ConfigContext.Provider value={config}>
             <React.StrictMode>
-                <App />
+                <App/>
             </React.StrictMode>
         </ConfigContext.Provider>
     );
 };
 
-root.render(<RootComponent />);
+root.render(<RootComponent/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
