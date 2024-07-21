8a6763c2d187afa049549fb1279b33e0b56967f2
Mil0w0 <lhilderal@myges.fr>
2024-07-21 15:18:42 +0200

    fix payment displays

--- src/routes/users/MySubscription.tsx
+++ src/routes/users/MySubscription.tsx
@@ -40,6 +40,7 @@ export function MySubscription(){
     const [connectedAccountId, setConnectedAccountId] = useState("");
     const [subscription, setSubscription] = useState<Subscription>({});
     const [invoices, setInvoices] = useState([]);
+    const [payments, setPayments] = useState([]);
     const [isDataFetched, setIsDataFetched] = useState(false);
     const userSession = useContext(UserSessionContext)?.userSession;
     const [open, setOpen] = useState(false);
@@ -155,8 +156,9 @@ export function MySubscription(){
         getSubscription().then((data) => {
             setSubscription(data);
         })
-        getInvoices().then((invoices) => {
-            setInvoices(invoices);
+        getInvoices().then((data) => {
+            setInvoices(data.invoices);
+            setPayments(data.payments);
             setPageLoaded(true);
         });
 
@@ -223,11 +225,24 @@ export function MySubscription(){
                                     </TableHead>
                                     <TableBody>
                                         {
-                                            invoices && invoices.map((payment: any) => {
+                                            invoices && invoices.map((invoice: any) => {
+                                                return (
+                                                    <TableRow key={invoice.id}>
+                                                        <TableCell>{new Date(invoice.created * 1000).toLocaleDateString()}</TableCell>
+                                                        <TableCell>Cotisation</TableCell>
+                                                        <TableCell>{invoice.amount_paid/100} {invoice.currency}</TableCell>
+                                                        <TableCell>{invoice.status === "paid" ? "Finalisé": "Non-finalisé"}</TableCell>
+                                                        <TableCell><a href={invoice.invoice_pdf} target="_blank" rel="noreferrer"><Download color="primary"/></a></TableCell>
+                                                    </TableRow>
+                                                )
+                                            })
+                                        }
+                                        {
+                                            payments && payments.map((payment: any) => {
                                                 return (
                                                     <TableRow key={payment.id}>
                                                         <TableCell>{new Date(payment.created * 1000).toLocaleDateString()}</TableCell>
-                                                        <TableCell>{payment.description.toUpperCase().includes("SUBSCRIPTION") ? "Cotisation" : "Don"}</TableCell>
+                                                        <TableCell>Don</TableCell>
                                                         <TableCell>{payment.amount/100} {payment.currency}</TableCell>
                                                         <TableCell>{payment.status === "paid" ? "Finalisé": "Non-finalisé"}</TableCell>
                                                         <TableCell><a href={payment.invoice.invoice_pdf} target="_blank" rel="noreferrer"><Download color="primary"/></a></TableCell>
